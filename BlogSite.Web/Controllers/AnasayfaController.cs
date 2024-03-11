35d847e3bd3770431efb55832067c3abc50e1a3b
fazlı şahiner <fazlisahiner55@gmail.com>
2024-03-12 01:39:52 +0300

    Add project files.

---
+++ BlogSite.Web/Controllers/AnasayfaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Web.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        // GET: Anasayfa
+        //public ActionResult Index()
+        //{
+        //    return View();   bunu kullanmıyacaz
+        //}
+
+        public ActionResult AnasayfaIndex()
+        {
+            return View();
+        }
+    }
+}
