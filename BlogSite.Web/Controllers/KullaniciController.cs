52f8c26b78b98332af6ca2ec35a8a717100d2b36
fazlı şahiner <fazlisahiner55@gmail.com>
2024-03-29 17:09:54 +0300

    login arayüzü

---
+++ BlogSite.Web/Controllers/KullaniciController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Web.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        // GET: Kullanici
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GirisIndex(string kullaniciAdi,string sifre)
+        {
+            MyBlogSiteDBEntities db = new MyBlogSiteDBEntities();
+            var getirKullanici = db.Users.Where(k => k.UserName == kullaniciAdi && k.UserPassword == sifre).FirstOrDefault();
+            if (getirKullanici!=null)
+            {
+                //giriş
+
+            }
+            else
+            {
+                //uyarı mesajı
+            }
+            //Login 
+            return View();
+        }
+
+        public ActionResult KullaniciKayitIndex()
+        {
+            return View();
+        }
+    }
+}
