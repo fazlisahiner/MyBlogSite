fa01c54488fc2c4b16b5bc644931bac6966b01b4
fazlı şahiner <fazlisahiner55@gmail.com>
2024-04-03 22:17:16 +0300

    register and log in

--- BlogSite.Web/Controllers/KullaniciController.cs
+++ BlogSite.Web/Controllers/KullaniciController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,27 +15,89 @@ namespace BlogSite.Web.Controllers
             return View();
         }
 
+        MyBlogSiteDBEntities db = new MyBlogSiteDBEntities();
 
-        public ActionResult GirisIndex(string kullaniciAdi,string sifre)
+        //public ActionResult GirisIndex(string kullaniciAdi,string sifre)
+        //{
+        //    MyBlogSiteDBEntities db = new MyBlogSiteDBEntities();
+        //    var getirKullanici = db.Users.Where(k => k.UserName == kullaniciAdi && k.UserPassword == sifre).FirstOrDefault();
+        //    if (getirKullanici!=null)
+        //    {
+        //        //giriş=> Anasayfaya git
+        //        Session["Yetki"] = kullaniciAdi;
+        //        return Redirect("/Anasayfa/AnasayfaIndex");
+        //    }
+        //    else
+        //    {
+        //        //uyarı mesajı
+        //        Console.WriteLine("Kullanıcı bulunamadı! ");
+        //        Thread.Sleep(3000);
+        //        return View();
+        //    }
+        //    //Login 
+
+        //}
+
+        public ActionResult GirisIndex()
         {
-            MyBlogSiteDBEntities db = new MyBlogSiteDBEntities();
-            var getirKullanici = db.Users.Where(k => k.UserName == kullaniciAdi && k.UserPassword == sifre).FirstOrDefault();
-            if (getirKullanici!=null)
-            {
-                //giriş
+            
+                return View();
+            
 
+        }
+
+        [HttpPost]
+        public ActionResult GirisIndex(FormCollection fc)
+        {
+            string email = fc["email"];
+            string sifre = fc["sifre"];
+
+            //MyBlogSiteDBEntities db = new MyBlogSiteDBEntities();
+            var getirKullanici = db.Users.Where(k => k.EMail == email && k.UserPassword == sifre).FirstOrDefault();
+            if (getirKullanici != null)
+            {
+                //giriş=> Anasayfaya git
+                Session["Yetki"] = email;
+                return Redirect("/Anasayfa/AnasayfaIndex");
             }
             else
             {
                 //uyarı mesajı
+               ViewBag.Message = "Kullanıcı adı veya şifre hatalı! ";
+                
+                Thread.Sleep(3000);
+                return View();
             }
             //Login 
-            return View();
+
         }
+
+
 
         public ActionResult KullaniciKayitIndex()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult KullaniciKayitIndex(FormCollection form)
+        {
+            MyBlogSiteDBEntities db = new MyBlogSiteDBEntities();
+                // bunu yazıp alt+shift+f10
+
+            Users model = new Users();
+            model.UserName = form["isimInput"].Trim();
+            model.EMail= form["emailInput"].Trim() ;
+            model.UserPassword= form["sifreInput"].Trim();
+
+            db.Users.Add(model);
+            db.SaveChanges();
+
+            return View();
+        }
+
+
     }
+
+    
 }
