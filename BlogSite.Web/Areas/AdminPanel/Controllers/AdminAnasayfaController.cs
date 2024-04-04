7cb1c5099850391598604cb492801095b9c4ee83
fazlı şahiner <fazlisahiner55@gmail.com>
2024-04-05 02:15:41 +0300

    Admin sayfası oluşturuldu

---
+++ BlogSite.Web/Areas/AdminPanel/Controllers/AdminAnasayfaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Web.Areas.AdminPanel.Controllers
+{
+    public class AdminAnasayfaController : Controller
+    {
+        // GET: AdminPanel/AdminAnasayfa
+        public ActionResult AnasayfaIndex()
+        {
+            return View();
+        }
+    }
+}
