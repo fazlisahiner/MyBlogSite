d1d31e80c88f9060d880b2f5a12b45b02f165e10
fazlı şahiner <fazlisahiner55@gmail.com>
2024-04-06 22:51:27 +0300

    Admin paneli için list ve insert

---
+++ BlogSite.Web/Areas/AdminPanel/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Web.Areas.AdminPanel.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: AdminPanel/Categories
+        public ActionResult CategoryIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogSite.Web/Areas/AdminPanel/Controllers/RolesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Web.Areas.AdminPanel.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: AdminPanel/Roles
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
